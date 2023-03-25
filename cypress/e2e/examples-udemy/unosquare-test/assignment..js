219ae6c1f487630babda0f3e3396d0d9ef39542a
Mariana Vives <mariana.vives@unosquare.com>
2023-03-24 18:49:13 -0600

    Configure cucumber

--- cypress/e2e/examples-udemy/unosquare-test/assignment..js
+++ cypress/e2e/examples-udemy/unosquare-test/assignment..js
@@ -1,7 +1,7 @@
 // Para la siguiente semana:
 
 // Instalar Cucumber
-// Re-hacer el mismo script en Gherking language
+// Re-hacer el mismo script en Gherkin language
 // Creando el feature file y el step definition file
 // Usar el pattern page object model
 // Crear el mismo flujo usando una data table y vamos a buscar 3 items (Macbook air, Iphone 14 pro max,, Samsung Galaxy S23)

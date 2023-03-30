17790e3858042225c83e53d7690c302611733dd4
Mariana Vives <mariana.vives@unosquare.com>
2023-03-30 09:53:25 -0600

    Changes still trying to iterate the menu and get and compare items

--- cypress/support/step_definitions/hamburguer-menu.js
+++ cypress/support/step_definitions/hamburguer-menu.js
@@ -21,13 +21,49 @@ When('I click on the left Menu', () => {
 Then(/^I verify that the elements on buscar por deparamento ([^"]*) and Programa y funcionalidades ([^"]*) are visible on the menu section$/, (dep, prog) => {
     //Iterate over the list until Buscar por Departamento
     hm_PO.verifyHamburguerMenuTitle();
+    //ShopByDpt
+    cy.get('#hmenu-content .hmenu.hmenu-visible .hmenu-item.hmenu-title').eq(1).invoke('text').as('shopByDpt')
+    //prog&feat
+    cy.get('#hmenu-content .hmenu.hmenu-visible .hmenu-item.hmenu-title').eq(2).invoke('text').as('progAndFeat')
+    //all Headers from the hamburguer menu
+    cy.get('#hmenu-content .hmenu.hmenu-visible .hmenu-item.hmenu-title').as('menuHeaders');
+    cy.get('#hmenu-content').as('menuContentsAll'); //imprime todos los elementos como un solo index y todo amontoado
 
-    cy.get('#hmenu-content .hmenu.hmenu-visible .hmenu-item.hmenu-title ').as('menuHeaders');
-    cy.get('@menuHeaders').each(($el, index, $list) => {
-        cy.log('My elements index: ' + index + " and its text" + $el.text())
-        cy.wrap($el)
+    // Xpath de Luis:
+    // cy.xpath("div[@id='hmenu-content']//child::div[.= 'search by deparment']//parent::li//following-sibling::li//a[@class = 'hmenu-item' and @data-menu-id < 10]");
 
+    //Esto rompe mi codigo y traba cypress.
+    // cy.get('#hmenu-content').then(($items)=>{
+    //     var i;
+    //     const listedItems = $items.text();
+
+    //     for(i=0; i< listedItems.length; i++){
+    //             cy.log(listedItems[i]);
+    //             cy.log("ShopByDptIdentified");
+    //     }
+    // })
+    cy.get('@menuContentsAll').each(($el, index, $list) => {
+        cy.log('My headers elements index: ' + index + " and its text" + $el.text());
+        // cy.get('@shopByDpt').then(($listShopByDpt) => {
+
+        // })
+        // if($el.text()==="@shopByDpt"){
+        //     cy.log("Elements from shop by Dpt: ")
+        //     cy.xpath('//li[contains(text(), "shop by department")]/following-sibling::li/')
+        // }
+
+        
+        // if ($el == "@shopByDpt"){
+        //     cy.log("Should say shop by dpt: " + $el.text())
+        // cy.wrap($el)
+        // } else if($el == "@progAndFeat"){
+        //     cy.wrap("Should say programs and features : " + $el)
+        // }
     })
+   
+
+
+    
 });
 
 
